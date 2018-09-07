744917912193b8aad4f5437f8976408fb16ce6fa
Joseph Vargas <joseph@cpokemon.com>
2018-09-06 18:12:49 -0600

    Version final

--- comandos.sql
+++ comandos.sql
@@ -1,4 +1,5 @@
--- PUNTO 1
+-- PUNTO 1 VERSIÓN 1
+
 -- Platillo más gustado por día específico
 SELECT Platillo.idPlatillo, Platillo.nombre, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Platillo
 INNER JOIN RatingPlatillo ON RatingPlatillo.idPlatillo = Platillo.idPlatillo
@@ -18,7 +19,7 @@ AND strftime('%Y', RatingPlatillo.fecha) = "2018"
 GROUP BY Platillo.idPlatillo
 ORDER BY puntajePromedio DESC LIMIT 1;
 
--- Platillo más gustado por semestre
+-- Platillo más gustado por semestre (verison 1)
 SELECT Platillo.idPlatillo, Platillo.nombre, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Platillo
 INNER JOIN RatingPlatillo ON RatingPlatillo.idPlatillo = Platillo.idPlatillo
 AND strftime('%m', RatingPlatillo.fecha) >= "07" -- Primer mes del semestre
@@ -27,18 +28,6 @@ AND strftime('%Y', RatingPlatillo.fecha) = "2018"
 GROUP BY Platillo.idPlatillo
 ORDER BY puntajePromedio DESC LIMIT 1;
 
-
--- ALTERNATIVA PARA RAUL, PLATO MAS GUSTADO DE CADA SEMESTRE
-SELECT semestre, idPlatillo, nombrePlatillo, MAX(puntajePromedio) AS puntajePromedio
-FROM (
-	SELECT Semestre.nombre as semestre, Platillo.idPlatillo, Platillo.nombre AS nombrePlatillo, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Semestre
-	INNER JOIN RatingPlatillo ON 
-		strftime('%s', RatingPlatillo.fecha) BETWEEN strftime('%s', Semestre.fechaInicio) AND strftime('%s', Semestre.fechaFinal)
-	INNER JOIN Platillo on Platillo.idPlatillo = RatingPlatillo.idPlatillo
-	GROUP BY Semestre.idSemestre, Platillo.idPlatillo
-	ORDER BY puntajePromedio DESC
-)
-GROUP BY semestre;
 
 
 -- Platilo más gustado de un restaurante específico en un día específico
@@ -56,6 +45,59 @@ ORDER BY puntajePromedio DESC LIMIT 1;
 
 -- Mismos cambios de arriba para seleccionar por semestre, año, etc
 
+
+-- Platilo más gustado de TODO
+SELECT Platillo.idPlatillo, Platillo.nombre, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Platillo
+INNER JOIN RatingPlatillo ON RatingPlatillo.idPlatillo = Platillo.idPlatillo
+GROUP BY Platillo.idPlatillo
+ORDER BY puntajePromedio DESC LIMIT 1;
+
+
+-- PUNTO 1 VERSIÓN 2
+-- CON REPORTES PARA CADA ELEMENTO
+
+-- Platillo más gustado de cada dia
+SELECT dia, idPlatillo, nombre, MAX(puntajePromedio) AS puntajePromedio
+FROM (
+	SELECT strftime('%m-%Y-%d', RatingPlatillo.fecha) AS dia, Platillo.idPlatillo, Platillo.nombre, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Platillo
+	INNER JOIN RatingPlatillo ON RatingPlatillo.idPlatillo = Platillo.idPlatillo
+	GROUP BY strftime('%m-%Y-%d', RatingPlatillo.fecha), Platillo.idPlatillo
+	ORDER BY puntajePromedio DESC
+	)
+GROUP BY dia;
+
+-- Platillo más gustado de cada mes
+SELECT mes, idPlatillo, nombre, MAX(puntajePromedio) AS puntajePromedio
+FROM (
+	SELECT strftime('%m-%Y', RatingPlatillo.fecha) AS mes, Platillo.idPlatillo, Platillo.nombre, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Platillo
+	INNER JOIN RatingPlatillo ON RatingPlatillo.idPlatillo = Platillo.idPlatillo
+	GROUP BY strftime('%m-%Y', RatingPlatillo.fecha), Platillo.idPlatillo
+	ORDER BY puntajePromedio DESC
+	)
+GROUP BY mes;
+
+-- Plato más gustado de cada semestre
+SELECT semestre, idPlatillo, nombrePlatillo, MAX(puntajePromedio) AS puntajePromedio
+FROM (
+	SELECT Semestre.nombre as semestre, Platillo.idPlatillo, Platillo.nombre AS nombrePlatillo, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Semestre
+	INNER JOIN RatingPlatillo ON 
+		strftime('%s', RatingPlatillo.fecha) BETWEEN strftime('%s', Semestre.fechaInicio) AND strftime('%s', Semestre.fechaFinal)
+	INNER JOIN Platillo on Platillo.idPlatillo = RatingPlatillo.idPlatillo
+	GROUP BY Semestre.idSemestre, Platillo.idPlatillo
+	ORDER BY puntajePromedio DESC
+)
+GROUP BY semestre;
+
+-- Plato más gustado de cada restaurante
+SELECT idRestaurante, nombreRestaurante, idPlatillo, nombrePlatillo, MAX(puntajePromedio) AS puntajePromedio
+FROM (
+	SELECT Restaurante.idRestaurante, Restaurante.nombre as nombreRestaurante, Platillo.idPlatillo, Platillo.nombre as nombrePlatillo, AVG(RatingPlatillo.puntaje) AS puntajePromedio FROM Restaurante
+	INNER JOIN TiempoComida ON TiempoComida.idRestaurante = Restaurante.idRestaurante
+	INNER JOIN RatingPlatillo ON RatingPlatillo.idTiempoComida = TiempoComida.idTiempoComida
+	INNER JOIN Platillo ON RatingPlatillo.idPlatillo = Platillo.idPlatillo
+	GROUP BY Restaurante.idRestaurante, Platillo.idPlatillo
+)
+GROUP BY idRestaurante
 
 
 -- PUNTO 2
@@ -117,6 +159,6 @@ ORDER BY puntajePromedio DESC;
 
 
 
-
-INSERT INTO RatingPlatillo(idPlatillo, idEstudiante, idTiempoComida, puntaje, fecha)
-VALUES (1, 3, 2, 4.5, datetime("now"));
+-- Ignorar esto
+/*INSERT INTO RatingPlatillo(idPlatillo, idEstudiante, idTiempoComida, puntaje, fecha)
+VALUES (1, 3, 2, 4.5, datetime("now"));*/
